ddc618559ba0465afa43ac1cac1aab3fdb99a61c
babangsund <babangsund@gmail.com>
2019-08-14 00:40:35 +0200

    add license preamble

--- src/withToastSnack.js
+++ src/withToastSnack.js
@@ -8,9 +8,9 @@ import type {AbstractComponent} from 'react';
 
 import ReactToastSnackContext from './ReactToastSnackContext';
 
-function withToastSnack<Config: {}>(
-  Component: AbstractComponent<Config>,
-): AbstractComponent<Config> {
+function withToastSnack<Config: {}, Instance>(
+  Component: AbstractComponent<Config, Instance>,
+): AbstractComponent<Config, Instance> {
   return React.forwardRef((props, ref) => (
     <ReactToastSnackContext.Consumer>
       {context => {
