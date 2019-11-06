2028772ee6b042581d5fa3406b902b4f49036599
babangsund <babangsund@gmail.com>
2019-11-06 02:39:05 +0100

    prettier config; ts index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as useDuration } from "./useDuration";
+export { default as useToastSnack } from "./useToastSnack";
+export { default as withToastSnack } from "./withToastSnack";
+export { default as ReactToastSnackContext } from "./ReactToastSnackContext";
+export { default as ReactToastSnackProvider } from "./ReactToastSnackProvider";
