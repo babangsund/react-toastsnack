9b8989c241da1af862018523fb31ed0dcd2fe0ca
babangsund <babangsund@gmail.com>
2019-11-06 02:44:09 +0100

    Custom ts declarations; tsconfig.json

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,7 +1,3 @@
-// @flow
-
-'use strict';
-
 export const DEFAULT_DELAY = 400;
 export const DEFAULT_OFFSET = 8;
 export const DEFAULT_HEIGHT = 48;
---
+++ src/types.d.ts
@@ -0,0 +1,68 @@
+type ActionType = 'enqueue' | 'dequeue' | 'update' | 'exited';
+
+type Action = {
+  queue: ToastSnackQueue;
+  type: ActionType;
+  input: ToastSnackCreate | ToastSnackUpdate;
+};
+
+type ToastSnackCreate = {
+  id?: string;
+} & ToastSnackProperties;
+
+type ToastSnackUpdate = {
+  id: string;
+} & ToastSnackProperties;
+
+type ToastSnackProperties = {
+  id: string;
+  open?: boolean;
+  height?: number;
+};
+
+type ToastSnack = {
+  id: string;
+  open: boolean;
+  height: number;
+};
+
+type Create = (input: ToastSnackCreate) => string | null;
+type Update = (input: ToastSnackUpdate) => void;
+
+type ToastSnackProvider = {
+  create: Create;
+  update: Update;
+};
+
+type ReactToastSnackProviderProps = {
+  max?: number;
+  children: Node;
+  delay?: number;
+  offset?: number;
+  height?: number;
+  dismiss?: boolean;
+  renderer: (arg: any) => any;
+  initial?: Array<ToastSnackCreate>;
+  methods: {
+    [key: string]: (
+      onCreate: Create,
+      onUpdate: Update
+    ) => ToastSnackCreate | ToastSnackUpdate;
+  };
+};
+
+type ToastSnackSettings = {
+  max?: number;
+  delay: number;
+  height: number;
+  offset: number;
+  dismiss: boolean;
+};
+
+interface ToastSnackQueue {
+  getLast(): ToastSnack | null;
+  getLength(): number;
+  getSettings(): ToastSnackSettings;
+  enqueue(onCreate: ToastSnackCreate): string;
+  dequeue(): ToastSnack | null;
+}
